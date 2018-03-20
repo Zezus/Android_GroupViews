b8471abef3cd46ac05210bfc90cd7730ef6ff56a
Zezus <rustam-sadvakaso@mail.ru>
2018-03-20 19:40:57 +0600

    init

--- app/src/main/java/com/example/groupviews/MainActivity.java
+++ app/src/main/java/com/example/groupviews/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.groupviews;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
